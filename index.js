6d81e38070104fa027ab83961fac11162aaa971c
theandrews <andrews8.wu@gmail.com>
2019-07-24 14:35:26 -0300

    fix functions

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ if (!process.env.SENTRY_URL) throw Error('Invalid Sentry DSN')
 Sentry.init({ dsn: process.env.SENTRY_URL, environment: process.env.NODE_ENV });
 
 const errorHandler = () => ({
-  middleware(err, req, res, next){
+  middleware: (err, req, res, next) => {
     logError(err);
     return next(err);
   },
-  logError(origin, err){
+  logError: (origin, err) => {
     if (!origin) throw Error('Origin is required (from where the error was trown)');
     if (!err) throw Error('An Error is necessary to log');
     const eventId = Sentry.captureException(err);
     debug(`service:error->${origin}-> enventId: ${eventId}`)('throws an error %o', err.message);
   }
-})
+});
 
 module.exports = errorHandler;
