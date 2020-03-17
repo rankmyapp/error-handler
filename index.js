973e917f291b89e742dfce484d0323e8121301b6
Leandro Haruki Futema <leandroharuki@gmail.com>
2020-03-17 17:39:44 -0300

    Refactoring arguments

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const production = process.env.NODE_ENV === 'production';
 
 if (production) Sentry.init({ dsn: process.env.SENTRY_URL, environment: process.env.NODE_ENV });
 
-const errorHandler = (blockList = []) => ({
+const errorHandler = ({ blockEvents = [] } = {}) => ({
   middleware(err, req, res, next) {
-    if(blockList.includes(err.status)) return next(err)
+    if(blockEvents.includes(err.status)) return next(err)
     const eventId = Sentry.captureException(err);
-    debug(`service:error->${req.protocol + '://' + req.get('host') + req.originalUrl}-> enventId: ${eventId}`)('throws an error %o', err.message);
+    debug(`service:error->${req.protocol + '://' + req.get('host') + req.originalUrl}-> eventId: ${eventId}`)('throws an error %o', err.message);
     return next(err);
   },
   logError(origin, err) {
     if (!origin) throw Error('Origin is required (from where the error was thrown)');
     if (!err) throw Error('An Error is necessary to log');
-    if(blockList.includes(err.status)) return;
+    if(blockEvents.includes(err.status)) return false;
     const eventId = Sentry.captureException(err);
-    debug(`service:error->${origin}-> enventId: ${eventId}`)('throws an error %o', err.message);
+    debug(`service:error->${origin}-> eventId: ${eventId}`)('throws an error %o', err.message);
     return true;
   },
 });
